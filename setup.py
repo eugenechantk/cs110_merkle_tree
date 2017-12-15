c7600e97e19107590ce3eb03b6bae9896574e601
eugenechantk <eugenechantk@gmail.com>
2017-12-15 22:43:43 +0900

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'cs110-final',
+  packages = ['cs110-final'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A Merkle Tree Python 2.7 Package ',
+  author = 'Eugene Chan',
+  author_email = 'eugene@minerva.kgi.edu',
+  url = 'https://github.com/eugenechantk/cs110-final', # use the URL to the github repo
+  download_url = 'https://github.com/eugenechantk/cs110-final/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['merkle tree', '2.7', 'python','cs110','minerva'], # arbitrary keywords
+  classifiers = [],
+)
